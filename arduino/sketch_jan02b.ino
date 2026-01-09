81e39734c8523c7bb9b7857d7b83c46dee45918b
Benjamin Joseph <benejoseph@gmail.com>
2026-01-08 19:56:26 -0800

    Not sure why but one revolution is N-1 teeth.

--- arduino/sketch_jan02b.ino
+++ arduino/sketch_jan02b.ino
@@ -3,18 +3,19 @@
 // Sefine feedback signal pin and servo control pin
 #define FEEDBACK_PIN 2
 #define SERVO_PIN 9
+#define NUM_SERVO_TEETH (25)
+#define NUM_TURNTABLE_TEETH (90)
+
+const static int32_t kNumServoTeeth = TOFIXEDPOINT(NUM_SERVO_TEETH - 1);
+const static int32_t kNumTurntableTeeth = TOFIXEDPOINT(NUM_TURNTABLE_TEETH - 1);
+const static int32_t kGearRatio = QDIV(kNumTurntableTeeth, kNumServoTeeth);
 
 // Set feedback signal pin number
 FeedBackServo servo = FeedBackServo(FEEDBACK_PIN);
 
-int target = 0;             // State selection
-const long kIntervalMillis = 5000; // 2 seconds (in milliseconds)
-unsigned long previousTimeMillis = 0;
-unsigned long tt = 0;
-int milliAccumulator = 0;
-int angleAtEpoch = 0;
+static char buffer[128];
+static char readBuffer[128];
 
-char buffer[128];
 
 void setup()
 {
@@ -24,65 +25,46 @@ void setup()
     servo.setServoControl(SERVO_PIN);
 
     // Adjust as needed.
-    servo.setKp(/*Kp=*/TOFIXEDPOINT(2.0),
-                /*Ki=*/TOFIXEDPOINT(4.0),
+    servo.setKp(/*Kp=*/TOFIXEDPOINT(4.0),
+                /*Ki=*/TOFIXEDPOINT(10.0),
                 /*Kd=*/TOFIXEDPOINT(0.00));
 
     servo.setThresholdDeg(TOFIXEDPOINT(4.0));
 
     servo.setTargetAngle(0);
 
+    snprintf(buffer, sizeof(buffer), "\n\n\nINIT\n");
+    Serial.print(buffer);
+    snprintf(buffer, sizeof(buffer), "servo_time_millis,turntable_angle_q8,turntable_target_angle_deg_q8,turntable_error_angle_deg_q8\n");
+    Serial.print(buffer);
+
 }
 
 void loop()
 {
+    if (Serial.available() > 0) {
+      memset(readBuffer, 0, sizeof(readBuffer));
+      int bytesRead = Serial.readBytes(readBuffer, sizeof(readBuffer));
+      int newTarget = 0;
+      if (sscanf(readBuffer, "%d", &newTarget) > 0) {
+          int32_t targetAngle = ((int32_t)(newTarget)) << Q_FIXEDPOINT;
+          servo.setTargetAngle(QMUL(targetAngle, kGearRatio));
+          //snprintf(buffer, sizeof(buffer), "set target to %d deg\n", newTarget);
+          //Serial.print(buffer);
+      }
+    }
+    
     // Calculate whether new target input request meets specified time interval requirement to prevent mistarget
     const unsigned long currentTimeMillis = millis();
     
     // Rotate servo from 0 to 180 (w/ +-2 threshold) using non-blocking.
     servo.update(currentTimeMillis);
 
-    // Retrieve angle from servo
-    const int32_t currentAngle = servo.getAngle();
-
-#if 0
-    unsigned long dtMillis = currentTimeMillis - tt;
-    tt = currentTimeMillis;
-    milliAccumulator += dtMillis;
-    
-    if (milliAccumulator > 100) {
-        int deltaAngle = currentAngle - angleAtEpoch;
-        angleAtEpoch = currentAngle;
-        snprintf(buffer, sizeof(buffer), "%d %d\n", milliAccumulator, deltaAngle);
-        Serial.print(buffer);
-        milliAccumulator = 0;
-    }
-#endif
-
     snprintf(buffer, sizeof(buffer), "%"PRIu32",%"PRId32", %"PRId32", %"PRId32"\n", 
-        currentTimeMillis - previousTimeMillis, 
-        servo.getTargetAngle(),
-        servo.getErrorAngle(), 
-        servo.getErrorIntegral());
-
+        currentTimeMillis,
+        QDIV(servo.getAngle(), kGearRatio),
+        QMUL(servo.getTargetAngle(), kGearRatio),
+        QDIV(servo.getErrorAngle(), kGearRatio));
     Serial.print(buffer);
-    //Serial.println(currentAngle, currentTimeMillis);
 
-    if (currentTimeMillis - previousTimeMillis >= kIntervalMillis)
-    {
-        previousTimeMillis = currentTimeMillis;
-
-        // Prevents improper targetting by providing proper time for relevant calculations to take place
-        switch (target)
-        {
-        case 0:
-            target = 1;
-            servo.setTargetAngle(0);
-            break;
-        case 1:
-            target = 0;
-            servo.setTargetAngle(TOFIXEDPOINT(720));
-            break;
-        }
-    }
 }
